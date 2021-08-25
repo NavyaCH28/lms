eb7b17ba4da2cf37de31958b4b597db306ffc03d
Navya sree <sreenavya786@gmail.com>
2021-08-25 23:38:28 +0530

    test case changes
    test case changes

--- src/main/java/com/hcl/lms/repository/LMSRepository.java
+++ src/main/java/com/hcl/lms/repository/LMSRepository.java
@@ -1,7 +1,5 @@
 package com.hcl.lms.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,7 +10,6 @@ import com.hcl.lms.model.Book;
 @Repository
 public interface LMSRepository extends JpaRepository<Book, Long>,JpaSpecificationExecutor<Book> {
 
-	//List<Book> findByTitleAndCategory(String title,String Category);
 	
 	static Specification<Book>  hasTitle(String title){
 		return (book, cq, cb) -> cb.equal(book.get("title"), title);
--- src/test/java/com/hcl/lms/controller/LMSControllerTest.java
+++ src/test/java/com/hcl/lms/controller/LMSControllerTest.java
@@ -2,16 +2,19 @@ package com.hcl.lms.controller;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -22,6 +25,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.hcl.lms.dto.BookDTO;
+import com.hcl.lms.model.Book;
 import com.hcl.lms.repository.LMSRepository;
 import com.hcl.lms.service.LMSService;
 
@@ -69,14 +73,34 @@ public class LMSControllerTest  {
 	MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 	MockHttpServletResponse response = result.getResponse();
 	assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatus());
-
-	
 	}
 	
+	
+
+	@Test
+	public void searchBooks() throws Exception {
+		Mockito.when(lMSRepository.findAll(Mockito.any(Specification.class))).thenReturn(getBookEntity());
+		Mockito.when(lMSService.findByTitleAndCategory(Mockito.anyString(),Mockito.anyString())).thenReturn(getBookDTO());
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/search?title=Java&category=programming").accept(
+				MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		System.out.println(result.getResponse());
+		String expected = "[{title:Java,author:Mark,category:programming,price:100,quantity:3}]";
+		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);	
+	}
 	private List<BookDTO> getBookDTO(){
 		BookDTO mockBookDTO = new BookDTO("Java","Mark","programming",100l,3l);
 		List<BookDTO> list = new ArrayList<BookDTO>();
 		list.add(mockBookDTO);
+		return list;
+		}
+	private List<Book> getBookEntity(){
+		Book mockBook = new Book(1,"Java","Mark","programming",100l,3l);
+		List<Book> list = new ArrayList<Book>();
+		list.add(mockBook);
 		return list;
 		}
 }
